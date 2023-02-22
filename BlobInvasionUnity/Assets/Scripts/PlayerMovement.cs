512cf565258d9c7a21b61d29a7bf9a87f7ca905e
DaniilCoolUkraine <cd0games2000@gmail.com>
2023-02-22 14:05:50 +0200

    added joystick reader

--- BlobInvasionUnity/Assets/Scripts/PlayerMovement.cs
+++ BlobInvasionUnity/Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,28 @@ namespace BlobInvasion
 {
     public class PlayerMovement : MonoBehaviour
     {
+        [SerializeField] private FloatingJoystick _joystick;
+        [SerializeField] private float _speed;
+
+        private Rigidbody rb;
         
+        private float _inputY;
+        private float _inputX;
+
+        private void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        private void Update()
+        {
+            _inputX = _joystick.Horizontal * _speed;
+            _inputY = _joystick.Vertical * _speed;
+        }
+
+        private void FixedUpdate()
+        {
+            rb.velocity = new Vector3(_inputX, 0, _inputY);
+        }
     }
 }
