291df275ad39e1323b6fe1bb5f5ee36b734d9255
Andrii Maksymov <making714@gmail.com>
2023-02-22 17:42:02 +0200

    added code review of PlayerMovement

--- BlobInvasionUnity/Assets/Scripts/PlayerMovement.cs
+++ BlobInvasionUnity/Assets/Scripts/PlayerMovement.cs
@@ -7,18 +7,23 @@ namespace BlobInvasion
         [SerializeField] private FloatingJoystick _joystick;
         [SerializeField] private float _speed;
 
+        //code style _
         private Rigidbody rb;
-        
+
+        //remove fields
         private float _inputY;
         private float _inputX;
 
+        //GetComponent change to [SerializeField]
         private void Start()
         {
             rb = GetComponent<Rigidbody>();
         }
 
+        //remove Update
         private void Update()
         {
+            //remove fields
             _inputX = _joystick.Horizontal;
             _inputY = _joystick.Vertical;
         }
@@ -26,6 +31,8 @@ namespace BlobInvasion
         private void FixedUpdate()
         {
             var _direction = new Vector3(_inputX, 0, _inputY).normalized;
+
+            //move down to if
             rb.velocity = _direction * _speed;
 
             if (_direction != Vector3.zero)
