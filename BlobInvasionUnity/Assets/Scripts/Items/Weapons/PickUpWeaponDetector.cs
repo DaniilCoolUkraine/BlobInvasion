11163c325435fafd6bb1369bd7de03a5d57b3cf8
DaniilCoolUkraine <cd0games2000@gmail.com>
2023-03-01 12:31:28 +0200

    added possibilyty to pick up weapon

--- BlobInvasionUnity/Assets/Scripts/Items/Weapons/PickUpWeaponDetector.cs
+++ BlobInvasionUnity/Assets/Scripts/Items/Weapons/PickUpWeaponDetector.cs
@@ -1,9 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 namespace BlobInvasion.Items.Weapons
 {
     public class PickUpWeaponDetector : MonoBehaviour
     {
+        [SerializeField] private Transform _parentBone;
+        [SerializeField] private MultiParentConstraint _parentConstraint;
         
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                transform.SetParent(_parentBone);
+                transform.localPosition = Vector3.zero;
+                transform.localRotation = Quaternion.identity;
+                _parentConstraint.weight = 1;
+            }
+        }
     }
 }
