f920a187af72b4f8790523ecad66bb90b808fcf3
DaniilCoolUkraine <cd0games2000@gmail.com>
2023-03-11 14:03:39 +0200

    added enemy movement (player following)

---
+++ BlobInvasionUnity/Assets/Scripts/Enemies/EnemyMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace BlobInvasion.Enemies
+{
+    public class EnemyMovement : MonoBehaviour
+    {
+        //todo cannot be set in prefab (ask about it) 
+        //[SerializeField] private Transform _player;
+
+        [SerializeField] private float _speed;
+
+        private static Transform _player;
+
+        private void Start()
+        {
+            if (_player == null)
+            {
+                Debug.Log("a");
+                _player = GameObject.FindWithTag("Player").transform;
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            transform.position =
+                Vector3.MoveTowards(transform.position, _player.position, _speed * Time.fixedDeltaTime);
+            transform.LookAt(_player);
+        }
+    }
+}
