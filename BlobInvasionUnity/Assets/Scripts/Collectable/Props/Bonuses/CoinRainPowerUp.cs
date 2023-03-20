48a349ccd4d03c44fe752ed1dcdb57c710c584f6
DaniilCoolUkraine <cd0games2000@gmail.com>
2023-03-20 20:38:02 +0200

    added coinsRain powerUp

---
+++ BlobInvasionUnity/Assets/Scripts/Collectable/Props/Bonuses/CoinRainPowerUp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using BlobInvasion.Collectable.Props.Bonuses.ScriptableObjects;
+using BlobInvasion.Player;
+using UnityEngine;
+
+namespace BlobInvasion.Collectable.Props.Bonuses
+{
+    public class CoinRainPowerUp : PowerUp
+    {
+        private CoinRainPowerUpDataSO _coinRainPowerUpDataSO;
+        
+        private void Awake()
+        {
+            _coinRainPowerUpDataSO = (CoinRainPowerUpDataSO) _powerUpData;
+        }
+
+        protected override IEnumerator PowerUpCharacteristic(PlayerController playerController)
+        {
+            for (int i = 0; i < _coinRainPowerUpDataSO.PowerMultiplier; i++)
+            {
+                yield return new WaitForSeconds(_coinRainPowerUpDataSO.PoweringTime);
+                
+                Rigidbody coin = Instantiate(_coinRainPowerUpDataSO.Coin, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+                coin.AddExplosionForce(_coinRainPowerUpDataSO.ExplosionStrength, this.transform.position, _coinRainPowerUpDataSO.ExplosionStrength, _coinRainPowerUpDataSO.ExplosionStrength, ForceMode.Impulse);
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ BlobInvasionUnity/Assets/Scripts/Collectable/Props/Bonuses/ScriptableObjects/CoinRainPowerUpDataSO.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace BlobInvasion.Collectable.Props.Bonuses.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "NewCoinRainPowerUpData", menuName = "ScriptableObjects/CoinRainPowerUpData", order = 0)]
+    public class CoinRainPowerUpDataSO : PowerUpDataSO
+    {
+        [SerializeField] private GameObject _coin;
+
+        [SerializeField] private float _explosionStrength;
+       
+        public GameObject Coin => _coin;
+        
+        public float ExplosionStrength => _explosionStrength;
+        
+    }
+}
