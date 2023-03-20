bde40bde9785e45fea41cc4bb9869242d5362bf2
DaniilCoolUkraine <cd0games2000@gmail.com>
2023-03-20 17:55:07 +0200

    updated materials, baked light

---
+++ BlobInvasionUnity/Assets/Scripts/Collectable/Props/Bonuses/ScriptableObjects/PowerUpDataSO.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace BlobInvasion.Collectable.Props.Bonuses.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "NewPowerUpData", menuName = "ScriptableObjects/PowerUpData", order = 0)]
+    public class PowerUpDataSO : ScriptableObject
+    {
+        [SerializeField] private float _powerMultiplier;
+        [SerializeField] private float _poweringTime;
+
+        public float PowerMultiplier => _powerMultiplier;
+        public float PoweringTime => _poweringTime;
+    }
+}
